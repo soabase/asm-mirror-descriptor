bd5aa851aab0f60d347711619c65c776bd161159
randgalt <jordan@jordanzimmerman.com>
2019-03-19 06:28:47 -0500

    annotation support continues

--- asm-mirror-descriptor/src/main/java/io/soabase/asm/mirror/descriptor/AnnotationMirrorValueVisitor.java
+++ asm-mirror-descriptor/src/main/java/io/soabase/asm/mirror/descriptor/AnnotationMirrorValueVisitor.java
@@ -1,104 +1,128 @@
-/**
- * Copyright 2019 Jordan Zimmerman
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.soabase.asm.mirror.descriptor;
 
 import org.objectweb.asm.AnnotationVisitor;
 
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.AnnotationValueVisitor;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static io.soabase.asm.mirror.descriptor.MirrorSignatureReader.Mode.DESCRIPTOR;
 
-public class AnnotationMirrorValueVisitor {
+public class AnnotationMirrorValueVisitor implements AnnotationValueVisitor<Void, AnnotationVisitor> {
     private final String name;
     private final MirrorSignatureReader signatureReader;
-    private final AnnotationVisitor visitor;
 
-    public AnnotationMirrorValueVisitor(String name, MirrorSignatureReader signatureReader, AnnotationVisitor visitor) {
+    public AnnotationMirrorValueVisitor(String name, MirrorSignatureReader signatureReader) {
         this.name = name;
         this.signatureReader = signatureReader;
-        this.visitor = visitor;
     }
 
-    public void visit(Object value) {
-        if (value != null) {
-            if (value instanceof VariableElement) {
-                VariableElement variableElement = (VariableElement) value;
-                switch (variableElement.getKind()) {
-                    case ENUM_CONSTANT: {
-                        visitor.visitEnum(name, signatureReader.type(variableElement.asType(), DESCRIPTOR), variableElement.getSimpleName().toString());
-                        break;
-                    }
+    @Override
+    public Void visit(AnnotationValue av, AnnotationVisitor visitor) {
+        return null;
+    }
 
-                    case ANNOTATION_TYPE: {
-                         break;
-                    }
+    @Override
+    public Void visit(AnnotationValue av) {
+        return null;
+    }
 
-                    default: {
-                        visitor.visit(name, variableElement.getSimpleName().toString());
-                        break;
-                    }
-                }
-            } else if (value instanceof AnnotationValue) {
-                visit(((AnnotationValue)value).getValue());
-            } else {
-                Class<?> clazz = value.getClass();
-                if (clazz.isArray()) {
-                    visitArray(Stream.of());
-                } else if (List.class.isAssignableFrom(clazz)) {
-                    //noinspection unchecked
-                    visitArray(((List) value).stream());
-                } else {
-                    visitor.visit(name, value);
-                }
-            }
+    @Override
+    public Void visitBoolean(boolean b, AnnotationVisitor visitor) {
+        visitor.visit(name, b);
+        return null;
+    }
+
+    @Override
+    public Void visitByte(byte b, AnnotationVisitor visitor) {
+        visitor.visit(name, b);
+        return null;
+    }
+
+    @Override
+    public Void visitChar(char c, AnnotationVisitor visitor) {
+        visitor.visit(name, c);
+        return null;
+    }
+
+    @Override
+    public Void visitDouble(double d, AnnotationVisitor visitor) {
+        visitor.visit(name, d);
+        return null;
+    }
+
+    @Override
+    public Void visitFloat(float f, AnnotationVisitor visitor) {
+        visitor.visit(name, f);
+        return null;
+    }
+
+    @Override
+    public Void visitInt(int i, AnnotationVisitor visitor) {
+        visitor.visit(name, i);
+        return null;
+    }
+
+    @Override
+    public Void visitLong(long i, AnnotationVisitor visitor) {
+        visitor.visit(name, i);
+        return null;
+    }
+
+    @Override
+    public Void visitShort(short s, AnnotationVisitor visitor) {
+        visitor.visit(name, s);
+        return null;
+    }
+
+    @Override
+    public Void visitString(String s, AnnotationVisitor visitor) {
+        visitor.visit(name, s);
+        return null;
+    }
+
+    @Override
+    public Void visitType(TypeMirror t, AnnotationVisitor visitor) {
+        return null;
+    }
+
+    @Override
+    public Void visitEnumConstant(VariableElement c, AnnotationVisitor visitor) {
+        String descriptor = signatureReader.type(c.asType(), DESCRIPTOR);
+        visitor.visitEnum(name, descriptor, c.getSimpleName().toString());
+        return null;
+    }
+
+    @Override
+    public Void visitAnnotation(AnnotationMirror a, AnnotationVisitor visitor) {
+        String descriptor = signatureReader.type(a.getAnnotationType(), DESCRIPTOR);
+        AnnotationVisitor visitAnnotation = visitor.visitAnnotation(name, descriptor);
+        if (visitAnnotation != null) {
+            a.getElementValues().forEach((element, value) -> {
+                AnnotationMirrorValueVisitor visitAnnotationValues = new AnnotationMirrorValueVisitor(element.getSimpleName().toString(), signatureReader);
+                value.accept(visitAnnotationValues, visitAnnotation);
+            });
+            visitAnnotation.visitEnd();
         }
+        return null;
     }
 
-    private void visitArray(Stream<Object> values) {
+    @Override
+    public Void visitArray(List<? extends AnnotationValue> vals, AnnotationVisitor visitor) {
         AnnotationVisitor visitArray = visitor.visitArray(name);
         if (visitArray != null) {
-            AnnotationMirrorValueVisitor visitArrayMirror = new AnnotationMirrorValueVisitor(null, signatureReader, visitArray);
-            values.forEach(visitArrayMirror::visit);
+            AnnotationMirrorValueVisitor visitArrayValues = new AnnotationMirrorValueVisitor(null, signatureReader);
+            vals.forEach(v -> v.accept(visitArrayValues, visitArray));
             visitArray.visitEnd();
         }
+        return null;
     }
 
-    @SuppressWarnings("RedundantCast")
-    private void visitArray(Object value) {
-        if (value instanceof byte[]) {
-            visitArray(Stream.of((byte[])value));
-        } else if (value instanceof boolean[]) {
-            visitArray(Stream.of((boolean[])value));
-        } else if (value instanceof short[]) {
-            visitArray(Stream.of((short[])value));
-        } else if (value instanceof char[]) {
-            visitArray(Stream.of((char[])value));
-        } else if (value instanceof int[]) {
-            visitArray(Stream.of((int[])value));
-        } else if (value instanceof long[]) {
-            visitArray(Stream.of((long[])value));
-        } else if (value instanceof float[]) {
-            visitArray(Stream.of((float[])value));
-        } else if (value instanceof double[]) {
-            visitArray(Stream.of((double[])value));
-        } else {
-            visitArray(Stream.of((Object[])value));
-        }
+    @Override
+    public Void visitUnknown(AnnotationValue av, AnnotationVisitor visitor) {
+        return null;
     }
 }
--- asm-mirror-descriptor/src/main/java/io/soabase/asm/mirror/descriptor/ClassMirrorReader.java
+++ asm-mirror-descriptor/src/main/java/io/soabase/asm/mirror/descriptor/ClassMirrorReader.java
@@ -20,15 +20,16 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.TypeReference;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.TypeParameterElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -97,9 +98,9 @@ public class ClassMirrorReader {
                 .map(this::unwrapType)
                 .toArray(TypeMirror[]::new);
         TypeMirror[] typeParameters = method.getTypeParameters().stream()
-                .map(TypeParameterElement::asType)
+                .map(this::unwrapType)
                 .toArray(TypeMirror[]::new);
-        String descriptor = signatureReader.methodType(typeParameters, parameters, method.getReturnType(), DESCRIPTOR);
+        String descriptor = signatureReader.methodType(typeParameters, parameters, unwrapType(method.getReturnType()), DESCRIPTOR);
         String signature = Util.hasTypeArguments(method) ? signatureReader.methodType(typeParameters, parameters, method.getReturnType(), SIGNATURE) : null;
         String[] exceptions = readExceptions(method);
         MethodVisitor methodVisitor = classVisitor.visitMethod(accessFlags, methodName, descriptor, signature, exceptions);
@@ -107,6 +108,11 @@ public class ClassMirrorReader {
             method.getAnnotationMirrors().forEach(annotation -> {
                 String annotationDescriptor = signatureReader.type(annotation.getAnnotationType(), DESCRIPTOR);
                 AnnotationVisitor annotationVisitor = methodVisitor.visitAnnotation(annotationDescriptor, isVisibleAnnotation(annotation));
+                readAnnotationValue(annotation, annotationVisitor);
+            });
+            method.asType().getAnnotationMirrors().forEach(annotation -> {
+                String annotationDescriptor = signatureReader.type(annotation.getAnnotationType(), DESCRIPTOR);
+                AnnotationVisitor annotationVisitor = methodVisitor.visitTypeAnnotation(TypeReference.newTypeReference(TypeReference.METHOD_FORMAL_PARAMETER).getValue(), null, annotationDescriptor, isVisibleAnnotation(annotation));
                 readAnnotationValue(annotation, annotationVisitor);
             });
         }
@@ -126,10 +132,22 @@ public class ClassMirrorReader {
                 AnnotationVisitor annotationVisitor = fieldVisitor.visitAnnotation(annotationDescriptor, isVisibleAnnotation(annotation));
                 readAnnotationValue(annotation, annotationVisitor);
             });
+            field.asType().getAnnotationMirrors().forEach(annotation -> {
+                String annotationDescriptor = signatureReader.type(annotation.getAnnotationType(), DESCRIPTOR);
+                AnnotationVisitor annotationVisitor = fieldVisitor.visitTypeAnnotation(TypeReference.newTypeReference(TypeReference.FIELD).getValue(), null, annotationDescriptor, isVisibleAnnotation(annotation));
+                readAnnotationValue(annotation, annotationVisitor);
+            });
         }
     }
 
-    private TypeMirror unwrapType(VariableElement element) {
+    private TypeMirror unwrapType(TypeMirror type) {
+        if (type.getKind() == TypeKind.DECLARED) {
+            return unwrapType(((DeclaredType) type).asElement());
+        }
+        return type;
+    }
+
+    private TypeMirror unwrapType(Element element) {
         TypeMirror type;
         if (element.getAnnotationMirrors().isEmpty()) {
             type = element.asType();
@@ -150,8 +168,8 @@ public class ClassMirrorReader {
     private void readAnnotationValue(AnnotationMirror annotation, AnnotationVisitor annotationVisitor) {
         if (annotationVisitor != null) {
             annotation.getElementValues().forEach((element, annotationValue) -> {
-                AnnotationMirrorValueVisitor mirrorValueVisitor = new AnnotationMirrorValueVisitor(element.getSimpleName().toString(), signatureReader, annotationVisitor);
-                mirrorValueVisitor.visit(annotationValue.getValue());
+                AnnotationMirrorValueVisitor mirrorValueVisitor = new AnnotationMirrorValueVisitor(element.getSimpleName().toString(), signatureReader);
+                annotationValue.accept(mirrorValueVisitor, annotationVisitor);
             });
             annotationVisitor.visitEnd();
         }
